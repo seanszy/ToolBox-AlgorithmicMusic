446cffebc5e480ec9f0e45c57173b0b9c7b73b86
Sean Szymanski <sean.szymanski@students.olin.edu>
2017-03-25 18:31:30 -0400

    Blues Toolbox

--- blues_solo.py
+++ blues_solo.py
@@ -3,15 +3,12 @@
 import atexit
 import os
 from random import choice
-
 from psonic import *
 
 # The sample directory is relative to this source file's directory.
 SAMPLES_DIR = os.path.join(os.path.dirname(__file__), "samples")
-
 SAMPLE_FILE = os.path.join(SAMPLES_DIR, "bass_D2.wav")
 SAMPLE_NOTE = D2  # the sample file plays at this pitch
-
 
 def play_note(note, beats=1, bpm=60, amp=1):
     """Plays note for `beats` beats. Returns when done."""
@@ -37,6 +34,34 @@ atexit.register(stop)  # stop all tracks when the program exits normally or is i
 
 # These are the piano key numbers for a 3-octave blues scale in A. See: http://en.wikipedia.org/wiki/Blues_scale
 blues_scale = [40, 43, 45, 46, 47, 50, 52, 55, 57, 58, 59, 62, 64, 67, 69, 70, 71, 74, 76]
-beats_per_minute = 45				# Let's make a slow blues solo
+beats_per_minute = 90				# Let's make a slow blues solo
 
-play_note(blues_scale[0], beats=1, bpm=beats_per_minute)
+def main():
+    curr_note = 0
+    licks = [[(1, 0.5), (1, 0.5), (1, 0.5), (1, 0.5)],
+    [(-1, 0.5), (-1, 0.5), (-1, 0.5), (-1, 0.5)],
+    [(-1, 0.25), (-1, 0.5), (1, 0.25), (-1, .75), (-1, .25)],
+    [(-1, 0.25), (-2, 0.5), (2, 0.25), (1, .5), (-1, .25), (-1, .25)],
+    [(1, 0.5 * 1.1), (1, 0.5 * 0.9), (1, 0.5 * 1.1), (1, 0.5 * 0.9)], #swing
+    [(-1, 0.5 * 1.1), (-2, 0.5 * 0.9), (1, 0.5 * 1.1), (2, 0.5 * 0.9)]] #swing
+    amps = 1
+    for _ in range(10):
+        random_number = random.choice(range(6)) #chooses random number for lick
+        amps = 1
+        if random_number == 3:
+            amps = 2
+        print (random_number)
+        lick = licks[random_number] #randomly chooses lick from licks
+        for note in lick:
+            curr_note += note[0]
+            #Makes the music jump to the cool note if it hits zero.
+            #This makes it sound much better
+            if curr_note < 0:
+                curr_note = 6
+            if curr_note > len(blues_scale)-1: #won't let it get too high
+                curr_note = len(blues_scale)-1
+            play_note(blues_scale[curr_note], note[1], beats_per_minute, amps) #plays note
+    play_note(blues_scale[0], beats=1, bpm=beats_per_minute) #ending note
+
+if __name__ == '__main__':
+    main()
